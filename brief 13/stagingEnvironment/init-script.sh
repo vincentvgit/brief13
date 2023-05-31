db83e168a2be26b60079bce05c96b42d947c208c
vincentvgit <127845446+vincentvgit@users.noreply.github.com>
2023-05-31 16:53:32 +0200

    Delete init-script.sh

--- brief 13/stagingEnvironment/init-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Installing Docker
-curl -fsSL https://get.docker.com -o install-docker.sh
-sudo sh install-docker.sh
-
-
-#Creating container
-sudo docker run -d -p 8080:80 vincentvdocker/mynginx2:latest
