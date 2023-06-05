d7fd3d8fbc97fb3350414d3e38c5279519a4e989
vincentvgit <127845446+vincentvgit@users.noreply.github.com>
2023-06-05 09:26:10 +0200

    Update outputs.tf

--- brief 13/stagingEnvironment/outputs.tf
+++ brief 13/stagingEnvironment/outputs.tf
@@ -9,3 +9,14 @@ output "environment" {
 output "Ressource_group_name" {
   value = module.Staging.Ressource_group_name
 }
+output "The_subnet_ID" {
+ value = module.Staging.The_subnet_ID
+}
+
+output "The_vnet_ID" {
+ value = module.Staging.The_vnet_ID
+}
+
+output "The_websrever_Private_ip" {
+   value = module.Staging.The_websrever_Private_ip
+}
