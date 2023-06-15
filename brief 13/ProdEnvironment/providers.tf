438016450faa7e7b252ff032542b36fccd51b6ee
vincentvgit <127845446+vincentvgit@users.noreply.github.com>
2023-06-15 14:32:10 +0200

    Update providers.tf

--- brief 13/ProdEnvironment/providers.tf
+++ brief 13/ProdEnvironment/providers.tf
@@ -9,6 +9,8 @@ terraform {
 provider "azurerm" {
    subscription_id = var.subscription_id
    tenant_id = var.tenant_id
+   client_id = "bd631d07-c4bc-4e32-bde8-943fe08ae1b9"
+  client_secret ="2Cq8Q~LhYvlLj7l4BRJ5Ekm4fP9S9F~KG0LGbbSN"
    skip_provider_registration = true
    features {}
 }
